4d0712bf33150b64f65362cca18adf6ba08044fa
Odysseas.eth <odyslam@gmail.com>
2023-05-12 11:59:57 +0200

    taud and ircd mvp

---
+++ ircd/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Create the default config file
+/usr/local/bin/ircd
+# Run ircd with default config 
+/usr/local/bin/ircd
---
+++ taud/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Create the default config file
+echo "taud is not configured with a workspace yet"
